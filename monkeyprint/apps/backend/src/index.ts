b268bfd7cc02f74ff5eeaa1252f87f1d90a84d0f
painor <pi.oussama@gmail.com>
2025-02-19 01:44:55 +0100

    refactor: restructure project and remove turborepo boilerplate
    This commit involves significant project restructuring:
    - Removed docs app and related configurations
    - Added backend app with Fastify server
    - Created new packages for db, utils
    - Simplified web app and removed external dependencies
    - Removed eslint and typescript config packages
    - Introduced a basic text formatting utility

---
+++ monkeyprint/apps/backend/src/index.ts
@@ -0,0 +1,28 @@
+import { formatText } from '@monkeyprint/utils/format-text';
+import Fastify from 'fastify';
+
+const fastify = Fastify({
+    logger: true
+});
+
+// Declare a route
+fastify.get('/', async function handler(request, reply) {
+    // get the query param "text"
+    const { text } = request.query as { text?: string };
+    if (!text) {
+        return reply.status(400).send({ error: 'text is required' });
+    }
+    return { text: formatText(text) };
+});
+
+// Run the server!
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3001 });
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
+};
+
+start();
