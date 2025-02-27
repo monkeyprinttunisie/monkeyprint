71fabc79fb3b7dc347cea4e5bed9af8cd8bb6ef0
Ahmed Zouaghi <ahmedzouaghi2003@gmail.com>
2025-02-27 15:20:44 +0100

    add listProducts file

---
+++ apps/web/app/products/listProducts/page.tsx
@@ -0,0 +1,29 @@
+import { listProducts } from "@/actions/productActions";
+
+export default async function ProductHome() {
+  const products = await listProducts();
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>{product.description}</td>
+              <td>{product.price.toString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
