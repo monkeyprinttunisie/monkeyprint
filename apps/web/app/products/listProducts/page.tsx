35ea80c73838e6c5c37486dd2f2779a974be7445
Ahmed Zouaghi <ahmedzouaghi2003@gmail.com>
2025-03-07 05:57:06 +0100

    add style

--- apps/web/app/products/listProducts/page.tsx
+++ apps/web/app/products/listProducts/page.tsx
@@ -1,43 +1,48 @@
 "use client";
 
-import Cart from "@/components/cart";
 import { AddToCartButton } from "@/components/addToCartButton";
 import DeleteProductButton from "@/components/DeleteProductButton";
 import { useProducts } from "@/context/ProductContext";
+import Menu from "@/components/Menu";
 
 export default function ProductHome() {
-  const {products, deleteProduct} = useProducts();
+  const { products, deleteProduct } = useProducts();
 
   return (
-    <div>
-      <h1>Products</h1>
-      <Cart />
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <td>{product.name}</td>
-              <td>{product.description}</td>
-              <td>{product.price.toString()}</td>
-              <td>
-                <AddToCartButton product={product} />
-                <DeleteProductButton
-                  productId={product.id}
-                  onDelete={deleteProduct}
-                />
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="bg-white h-[90vh] overflow-x-hidden">
+      <h1 className="ml-2 mt-4 font-raleway font-bold text-[21px] text-gray-800">
+        Products
+      </h1>
+      <div className="grid grid-cols-2 m-3 mt-0">
+        {products.map((product) => (
+          <div key={product.id} className="p-2 flex flex-col">
+            <div className="p-1.5 bg-white shadow-md shadow-[rgba(0,0,0,0.1)] rounded-[9px]">
+              <img
+                className="w-[100%] h-[26vh] rounded-[9px]"
+                src={product.imageUrl}
+                alt={product.name}
+              />
+            </div>
+            <div className="text-left">
+              <h2 className="mt-3 font-nunito font-normal text-[16px] leading-[16px] text-gray-800">
+                {product.name}
+              </h2>
+            </div>
+            <div className="flex items-center justify-between gap-4 mt-2">
+              <p className="font-raleway font-bold text-[17px] text-gray-800">
+                {product.price.toFixed(2)}dt
+              </p>
+              <AddToCartButton product={product} />
+            </div>
+            {/* <div className="">
+              <DeleteProductButton
+                productId={product.id}
+                onDelete={deleteProduct}
+              />
+            </div> */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
