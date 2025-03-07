a72cc799384a36e1d4477080ffb1b019541f7220
Ahmed Zouaghi <ahmedzouaghi2003@gmail.com>
2025-03-07 05:53:15 +0100

    create menu and add it to layout.tsx

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { CartProvider } from "@/context/CartContext";
 import { ProductProvider } from "@/context/ProductContext";
+import Menu from "./components/Menu";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,7 +28,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <CartProvider>
-          <ProductProvider>{children}</ProductProvider>
+          <ProductProvider>
+            {children}
+            <Menu />
+          </ProductProvider>
         </CartProvider>
       </body>
     </html>
