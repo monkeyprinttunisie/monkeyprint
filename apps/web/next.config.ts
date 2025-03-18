3c5ac0a6e483b11eb4d637c8d98673a0278a5cce
Rahma Maalej <rahma.maalej.13@gmail.com>
2025-03-18 15:32:40 +0100

    allowing external images

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,10 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
-
+const nextConfig: NextConfig = {
+    images: {
+        domains: ['c6ha9vzzh0.ufs.sh'],
+    },
+};
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
