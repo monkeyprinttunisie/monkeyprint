6aed86c9b768465f206021ee92d62dc21c89797a
Rahma Maalej <rahma.maalej.13@gmail.com>
2025-04-07 21:38:28 +0200

    updated designer_tool default page

--- apps/web/app/[locale]/designer_tool/page.tsx
+++ apps/web/app/[locale]/designer_tool/page.tsx
@@ -1,5 +1,4 @@
 import { useTranslations } from "next-intl";
-import ScrollableNav from "@/components/scrollableNavigationBar";
 export default function Page() {
     const t = useTranslations("HomePage");
     return (
